pwyf/2024-Index-indicator-definitions

commit dd7aa9d494ce8e692c00b59f8d55748d57bad3a3

test defns: Remove some 1.0x compatibility we don't need any more
These changes were already made in the 2018 Technical Paper appendix,
but hadn't been made here.

test_definitions/finance/11_commitment.feature
@@ -5,4 +5,4 @@ Feature: Commitment
     Given an IATI activity
      And the activity is current
      And `activity-status/@code` is one of 2, 3 or 4
-     Then `transaction[transaction-type/@code="2" or transaction-type/@code="C"]` should be present and of non-zero value
+     Then `transaction/transaction-type[@code="2"]` should be present and of non-zero value

test_definitions/project_attributes/14_title.feature
@@ -4,9 +4,9 @@ Feature: Title
   Scenario Outline: Title is present
     Given an IATI activity
      And the activity is current
-     Then `title[text() or narrative/text()]` should be present
+     Then `title/narrative/text()` should be present
 
   Scenario Outline: Title has at least 10 characters
     Given an IATI activity
      And the activity is current
-     Then `title/text() | title/narrative/text()` should have at least 10 characters
+     Then `title/narrative/text()` should have at least 10 characters

test_definitions/project_attributes/15_description.feature
@@ -4,9 +4,9 @@ Feature: Description
   Scenario Outline: Description is present
     Given an IATI activity
      And the activity is current
-     Then `description[text() or narrative/text()]` should be present
+     Then `description/narrative/text()` should be present
 
   Scenario Outline: Description has at least 80 characters
     Given an IATI activity
      And the activity is current
-     Then `description/text() | description/narrative/text()` should have at least 80 characters
+     Then `description/narrative/text()` should have at least 80 characters

test_definitions/project_attributes/16_planned_dates.feature
@@ -4,9 +4,9 @@ Feature: Planned dates
   Scenario Outline: Planned start date is present
     Given an IATI activity
      And the activity is current
-     Then `activity-date[@type="1" or @type="start-planned"]` should be present
+     Then `activity-date[@type="1"]` should be present
 
   Scenario Outline: Planned end date is present
     Given an IATI activity
      And the activity is current
-     Then `activity-date[@type="3" or @type="end-planned"]` should be present
+     Then `activity-date[@type="3"]` should be present

test_definitions/project_attributes/17_actual_dates.feature
@@ -5,10 +5,10 @@ Feature: Actual dates
     Given an IATI activity
      And the activity is current
      And `activity-status/@code` is one of 2, 3 or 4
-     Then `activity-date[@type="2" or @type="start-actual"]` should be present
+     Then `activity-date[@type="2"]` should be present
 
   Scenario Outline: Actual end date is present
     Given an IATI activity
      And the activity is current
      And `activity-status/@code` is one of 3 or 4
-     Then `activity-date[@type="4" or @type="end-actual"]` should be present
+     Then `activity-date[@type="4"]` should be present

tests/project_attributes/test_description.py
@@ -29,7 +29,7 @@ class TestDescription(TestCase):
 
         assert result is False
 
-    def test_description_narrative_is_present(self):
+    def test_description_is_present(self):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
@@ -44,7 +44,7 @@ class TestDescription(TestCase):
 
         assert result is True
 
-    def test_description_is_present(self):
+    def test_description_no_narrative(self):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
@@ -55,13 +55,15 @@ class TestDescription(TestCase):
         activity = etree.fromstring(xml)
         result = self.test(activity)
 
-        assert result is True
+        assert result is False
 
     def test_description_is_insufficiently_short(self):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
-          <description>A short description</description>
+          <description>
+            <narrative>A short description</narrative>
+          </description>
         </iati-activity>
         '''
 

tests/project_attributes/test_title.py
@@ -29,7 +29,7 @@ class TestTitle(TestCase):
 
         assert result is False
 
-    def test_title_narrative_is_present(self):
+    def test_title_is_present(self):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
@@ -44,7 +44,7 @@ class TestTitle(TestCase):
 
         assert result is True
 
-    def test_title_is_present(self):
+    def test_title_no_narrative(self):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
@@ -55,13 +55,15 @@ class TestTitle(TestCase):
         activity = etree.fromstring(xml)
         result = self.test(activity)
 
-        assert result is True
+        assert result is False
 
     def test_title_is_insufficiently_short(self):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
-          <title>Too short</title>
+          <title>
+            <narrative>Too short</narrative>
+          </title>
         </iati-activity>
         '''
 
@@ -75,7 +77,9 @@ class TestTitle(TestCase):
         xml = '''
         <iati-activity>
           <activity-status code="2"/>
-          <title>A much much longer title</title>
+          <title>
+            <narrative>A much much longer title</narrative>
+          </title>
         </iati-activity>
         '''
 

commit f9027c16eb0d20822acad1bf783c50ed0747ebae

test defns: Fix commitment test to not always fail
This was accidentally broken in:
https://github.com/pwyf/2024-Index-indicator-definitions/commit/dd7aa9d494ce8e692c00b59f8d55748d57bad3a3

test_definitions/finance/11_commitment.feature
@@ -5,4 +5,4 @@ Feature: Commitment
     Given an IATI activity
      And the activity is current
      And `activity-status/@code` is one of 2, 3 or 4
-     Then `transaction/transaction-type[@code="2"]` should be present and of non-zero value
+     Then `transaction[transaction-type/@code="2"]` should be present and of non-zero value

tests/finance/test_commitments.py
@@ -1,0 +1,98 @@
+from os.path import dirname, join, realpath
+from unittest import TestCase
+
+from bdd_tester import BDDTester
+from lxml import etree
+
+
+class TestCommitments(TestCase):
+    def setUp(self):
+        self.FILEPATH = dirname(realpath(__file__))
+        steps_path = join(self.FILEPATH, '..', '..', 'test_definitions',
+                          'step_definitions.py')
+        feature_path = join(self.FILEPATH, '..', '..', 'test_definitions',
+                            'finance',
+                            '11_commitment.feature')
+
+        tester = BDDTester(steps_path)
+        feature = tester.load_feature(feature_path)
+        self.test = feature.tests[0]
+
+    def test_commitment_no_value(self):
+        xml = '''
+        <iati-activity>
+          <activity-status code="2"/>
+          <transaction>
+            <transaction-type code="2"/>
+          </transaction>
+        </iati-activity>
+        '''
+
+        activity = etree.fromstring(xml)
+        result = self.test(activity)
+
+        assert result is False
+
+    def test_commitment_zero_value(self):
+        xml = '''
+        <iati-activity>
+          <activity-status code="2"/>
+          <transaction>
+            <transaction-type code="2"/>
+            <value>0</value>
+          </transaction>
+        </iati-activity>
+        '''
+
+        activity = etree.fromstring(xml)
+        result = self.test(activity)
+
+        assert result is False
+
+    def test_commitment_zero_value_decimal(self):
+        xml = '''
+        <iati-activity>
+          <activity-status code="2"/>
+          <transaction>
+            <transaction-type code="2"/>
+            <value>0.0</value>
+          </transaction>
+        </iati-activity>
+        '''
+
+        activity = etree.fromstring(xml)
+        result = self.test(activity)
+
+        assert result is False
+
+    def test_commitment_non_zero(self):
+        xml = '''
+        <iati-activity>
+          <activity-status code="2"/>
+          <transaction>
+            <transaction-type code="2"/>
+            <value>123.45</value>
+          </transaction>
+        </iati-activity>
+        '''
+
+        activity = etree.fromstring(xml)
+        result = self.test(activity)
+
+        assert result is True
+
+    def test_commitment_not_present(self):
+        xml = '''
+        <iati-activity>
+          <activity-status code="2"/>
+          <transaction>
+            <transaction-type code="3"/>
+          </transaction>
+        </iati-activity>
+        '''
+
+        activity = etree.fromstring(xml)
+        result = self.test(activity)
+
+        assert result is False
+

tests/finance/test_disbursements_and_expenditures.py
@@ -18,6 +18,22 @@ class TestDisbursementsAndExpenditures(TestCase):
         feature = tester.load_feature(feature_path)
         self.test = feature.tests[0]
 
+    def test_disbursement_or_expenditure(self):
+        xml = '''
+        <iati-activity>
+          <activity-status code="2"/>
+          <transaction>
+            <transaction-type code="{}"/>
+          </transaction>
+        </iati-activity>
+        '''
+
+        for transaction_type in ['3', '4']:
+            activity = etree.fromstring(xml.format(transaction_type))
+            result = self.test(activity)
+
+            assert result is True
+
     def test_disbursement_or_expenditure_non_zero(self):
         xml = '''
         <iati-activity>